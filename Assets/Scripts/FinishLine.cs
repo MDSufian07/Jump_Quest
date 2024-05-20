3cf05702907b42fad1c633b25555ed467e5bd594
Sufian <sufianabu206@gmail.com>
2024-05-20 16:04:31 +0600

    after complete finish line and some effect
    All most done level One just need to add some this like coin ,UI  etc

--- Assets/Scripts/FinishLine.cs
+++ Assets/Scripts/FinishLine.cs
@@ -1,33 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class FinishLine : MonoBehaviour
+public class Finish : MonoBehaviour
 {
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            LoadNextScene();
-        }
-    }
-
-    private void LoadNextScene()
-    {
-        int nextSceneIndex = SceneManager.GetActiveScene().buildIndex + 1;
-        if (nextSceneIndex < SceneManager.sceneCountInBuildSettings)
-        {
-            try
-            {
-                SceneManager.LoadScene(nextSceneIndex);
-            }
-            catch (System.Exception ex)
-            {
-                Debug.LogError("Failed to load next scene: " + ex.Message);
-            }
-        }
-        else
-        {
-            Debug.LogWarning("No next scene available to load.");
+            Debug.Log("Tuch the line");
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
         }
     }
 }
