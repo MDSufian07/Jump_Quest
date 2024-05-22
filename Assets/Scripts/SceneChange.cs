0f315ba7037616137d41879a16adcf1244a4e4d3
Sufian <sufianabu206@gmail.com>
2024-05-23 01:07:49 +0600

    after setup resume, quit, Level, etc button

--- Assets/Scripts/SceneChange.cs
+++ Assets/Scripts/SceneChange.cs
@@ -2,11 +2,46 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+
 public class SceneChange : MonoBehaviour
 {
+    [SerializeField] private GameObject panel;
+    public void StartMenu()
+    {
+        SceneManager.LoadScene("StartMenu");
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
     public void SceneStart()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void ShowPanel()
+    {
+        if (panel != null)
+        {
+            panel.SetActive(true); // Enable the panel
+        }
+    }
+
+    public void HidePanel()
+    {
+        if (panel != null)
+        {
+            panel.SetActive(false); // Disable the panel
+        }
+    }
+
+    public void LevelOne()
+    {
+        SceneManager.LoadScene("LevelOne");
+    }
+    public void LevelTwo()
+    {
+        SceneManager.LoadScene("Level Two");
     }
 
 }
