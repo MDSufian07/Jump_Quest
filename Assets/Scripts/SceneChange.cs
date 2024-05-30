aa5375b9f0ad1c1892cb4acfed3ad89779d34409
Sufian <sufianabu206@gmail.com>
2024-05-30 16:12:32 +0600

    after adding click sound

--- Assets/Scripts/SceneChange.cs
+++ Assets/Scripts/SceneChange.cs
@@ -6,16 +6,30 @@ using UnityEngine.SceneManagement;
 public class SceneChange : MonoBehaviour
 {
     [SerializeField] private GameObject panel;
+    public AudioSource ClickSound;
+
+    public float soundDelay = 0.2f; // Adjust this to the length of your click sound
+
+    private void PlaySound()
+    {
+        if (ClickSound != null && ClickSound.gameObject.activeInHierarchy && ClickSound.enabled)
+        {
+            ClickSound.Play();
+        }
+    }
     public void StartMenu()
     {
+        ClickSound.Play();
         SceneManager.LoadScene("StartMenu");
     }
     public void QuitGame()
     {
+        ClickSound.Play();
         Application.Quit();
     }
     public void SceneStart()
     {
+        ClickSound.Play();
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
@@ -23,7 +37,8 @@ public class SceneChange : MonoBehaviour
     {
         if (panel != null)
         {
-            panel.SetActive(true); // Enable the panel
+            PlaySound();
+            panel.SetActive(true);
         }
     }
 
@@ -31,16 +46,25 @@ public class SceneChange : MonoBehaviour
     {
         if (panel != null)
         {
-            panel.SetActive(false); // Disable the panel
+            PlaySound();
+            StartCoroutine(HidePanelAfterSound());
         }
+    }
+
+    private IEnumerator HidePanelAfterSound()
+    {
+        yield return new WaitForSeconds(ClickSound.clip.length);
+        panel.SetActive(false);
     }
 
     public void LevelOne()
     {
+        ClickSound.Play();
         SceneManager.LoadScene("LevelOne");
     }
     public void LevelTwo()
     {
+        ClickSound.Play();
         SceneManager.LoadScene("Level Two");
     }
 
